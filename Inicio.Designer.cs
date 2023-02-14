e3f0d7827dd301bacd74792bbf4bac6fcbebc6bc
Jhonny <juanchitorodi1312@gmail.com>
2023-02-13 21:51:04 -0600

    Agregar archivos de proyecto.

---
+++ Inicio.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Perceptron
+{
+    partial class Perceptron
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Perceptron));
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.AutoSize = true;
+            this.btnIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(51)))), ((int)(((byte)(47)))));
+            this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(164)))), ((int)(((byte)(115)))));
+            this.btnIngresar.Location = new System.Drawing.Point(68, 202);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(128, 46);
+            this.btnIngresar.TabIndex = 1;
+            this.btnIngresar.Text = "Iniciar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisX2.TitleFont = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY2.TitleFont = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(268, 12);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.LabelBorderWidth = 2;
+            series1.LabelForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 10;
+            series1.Name = "Validos";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series2.LabelBackColor = System.Drawing.Color.Red;
+            series2.LabelBorderWidth = 2;
+            series2.LabelForeColor = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.MarkerSize = 10;
+            series2.Name = "No validos";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Linea";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.MarkerSize = 10;
+            series4.Name = "Sin asignar";
+            this.chart2.Series.Add(series1);
+            this.chart2.Series.Add(series2);
+            this.chart2.Series.Add(series3);
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(532, 434);
+            this.chart2.TabIndex = 2;
+            this.chart2.Text = "chart1";
+            this.chart2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart2_MouseClick);
+            // 
+            // Perceptron
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(24)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(812, 458);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.btnIngresar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Perceptron";
+            this.Text = "Perceptron";
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Inicio.cs
@@ -0,0 +1,67 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using Perceptron.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Perceptron
+{
+    public partial class Perceptron : Form
+    {
+        private static List<double> puntosX = new List<double>();
+        private static List<double> puntosY = new List<double>();
+        private static List<double> puntosXN = new List<double>();
+        private static List<double> puntosYN = new List<double>();
+
+        private readonly static Percep perceptron = new Percep();
+
+        public Perceptron()
+        {
+            InitializeComponent();
+
+            //Maximos, minimos y linea cruzada
+            chart2.ChartAreas[0].AxisX.Minimum = -10;
+            chart2.ChartAreas[0].AxisY.Minimum = -10;
+            chart2.ChartAreas[0].AxisX.Maximum = 10;
+            chart2.ChartAreas[0].AxisY.Maximum = 10;
+            chart2.ChartAreas[0].AxisX.Crossing = 0;
+            chart2.ChartAreas[0].AxisY.Crossing = 0;
+
+            //Configuraciones extras de diseño
+            chart2.ChartAreas[0].AxisX.ArrowStyle = AxisArrowStyle.Triangle;
+            chart2.ChartAreas[0].AxisY.ArrowStyle = AxisArrowStyle.Triangle;
+            chart2.ChartAreas[0].AxisX.LineWidth = 2;
+            chart2.ChartAreas[0].AxisY.LineWidth = 2;
+            chart2.ChartAreas[0].AxisX.LabelAutoFitMinFontSize = 14;
+            chart2.ChartAreas[0].AxisY.LabelAutoFitMinFontSize = 14;
+
+            //Colores
+            chart2.Series[0].Color = Color.Green;
+            chart2.Series[1].Color = Color.Red;
+            chart2.Series[2].Color = Color.Black;
+            chart2.Series[3].Color = Color.Blue;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            perceptron.Entrenamiento();
+
+            return;
+        }
+
+        private void chart2_MouseClick(object sender, MouseEventArgs e)
+        {
+            puntosX.Add(chart2.ChartAreas[0].AxisX.PixelPositionToValue(e.X));
+            puntosY.Add(chart2.ChartAreas[0].AxisY.PixelPositionToValue(e.Y));
+
+            chart2.Series[3].Points.AddXY(puntosX.Last(), puntosY.Last());
+        }
+    }
+}
